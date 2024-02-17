fdf52c6e8dc251d8c5abedf6fde0052c3e60c74d
小叶 <1936472877@qq.com>
2024-02-17 22:14:37 +0800

    feat: 入群回调

--- Model/groupIncreaseMsg_default.js
+++ Model/groupIncreaseMsg_default.js
@@ -14,9 +14,24 @@
  * 只要保证最后导出的是一个 **要发送的消息的数组** 就行
  */
 
-//别改命名
+/**
+ * 需要发送的消息
+ */
 const msg = []
 
+/**
+ * 可选一: 直接导出msg
+ */
+// export default msg
 
-//别改导出,改了不能用后果自负
-export default msg
+/**
+ * 可选二: 传入参数group_id和user_id,然后返回msg
+ * @param {string} group_id 被拉入的群
+ * @param {string} user_id 邀请入群的用户
+ * @param {string} self_id 机器人账号
+ * @returns {Array} msg
+ */
+export default function (group_id, user_id, self_id) {
+    // 需要导出msg
+    return msg
+}
