332b93fe767684da2060df924e60763929e89538
小叶 <1936472877@qq.com>
2024-09-09 10:09:04 +0800

    🐞 fix: .

--- Model/web/http.js
+++ Model/web/http.js
@@ -4,15 +4,16 @@ import { getDauChartData, getWeekChartData, getcallStat } from './api.js'
 
 const path = '/qqbot'
 const corsOptions = {
-  'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
   'Access-Control-Allow-Headers': 'X-Requested-With,content-type,authorization',
   'Access-Control-Allow-Credentials': true
 }
 
-Bot.express.options(path + '/*', (req, res) => {
+Bot.express.use(path + '/*', (req, res, next) => {
   res.set(corsOptions)
-  res.sendStatus(200)
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
+  if (req.method === 'OPTIONS') return res.sendStatus(200)
+  next()
 })
 
 const token = {}
