bc17d7878c4f8e770556a58b5f22d3bf7d2a19a1
小叶 <1936472877@qq.com>
2024-12-13 10:17:47 +0800

    perf: 配置webhook验证增加日志

--- Model/webhook.js
+++ Model/webhook.js
@@ -96,6 +96,7 @@ export const runServer = async (onMessage, fastify = null) => {
         const keyPair = getKeyPair(appid, secret)
 
         const msg = `${body.d.event_ts}${body.d.plain_token}`
+        logger.error(`[QQBot-Plugin:Webhook-Server] 收到webhook配置验证请求: appid: ${appid} body: ${JSON.stringify(body.d, null, 2)}`)
 
         const signature = nodeForge.pki.ed25519.sign({
           message: Buffer.from(msg),
@@ -103,6 +104,7 @@ export const runServer = async (onMessage, fastify = null) => {
         })
 
         const signatureHex = Buffer.from(signature).toString('hex')
+        logger.error('[QQBot-Plugin:Webhook-Server] webhook配置验证结果: appid: ${appid} signature: ', signatureHex)
 
         return reply.send({
           plain_token: body.d.plain_token,
